sneedxynos/android_device_samsung_r8s

x1slte: initial device tree
Change-Id: Ie96634d5524f4b0a7673d1f8fb0e0f14c7a69949

BoardConfig.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common
+include device/samsung/universal990-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/x1slte
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := x1slte
+
+# inherit from the proprietary version
+-include vendor/samsung/x1slte/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,30 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/x1slte
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Inherit from common
+$(call inherit-product, device/samsung/universal990-common/device-common.mk)
+
+# Inherit from the proprietary version
+$(call inherit-product-if-exists, vendor/samsung/x1slte/x1slte-vendor.mk)

franklin-albuquerque/device_samsung_x1slte_POS

Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

BoardConfig.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common
+include device/samsung/universal9830-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/x1slte
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := x1slte
+
+# inherit from the proprietary version
+-include vendor/samsung/x1slte/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,30 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/x1slte
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Inherit from common
+$(call inherit-product, device/samsung/universal9830-common/device-common.mk)
+
+# Inherit from the proprietary version
+$(call inherit-product-if-exists, vendor/samsung/x1slte/x1slte-vendor.mk)