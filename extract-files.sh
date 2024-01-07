sneedxynos/android_device_samsung_r8s

x1slte: import common extract-utils
Change-Id: Ib84023abfefd8ac7127920e70a060660ff4d7e38

extract-files.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=x1slte
+export DEVICE_COMMON=universal990-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=x1slte
+export DEVICE_COMMON=universal990-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

franklin-albuquerque/device_samsung_x1slte_POS

Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

extract-files.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=x1slte
+export DEVICE_COMMON=universal9830-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

setup-makefiles.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=x1slte
+export DEVICE_COMMON=universal9830-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"